98750c52165da0456d8689ac82b1fac7e0541cbe
Jonathan Gilbert <github@jongilbert.com>
2026-07-10 15:11:39 -0700

    Music performance improvements

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ public let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/gistya/swift-compositional-init", from: "1.1.2"),
-        .package(url: "https://github.com/gistya/SwiftXState", exact: "2.0.0-alpha-4"),
+        .package(url: "https://github.com/gistya/SwiftXState", exact: "2.0.0-alpha-5"),
     ],
     targets: [
         .target(
